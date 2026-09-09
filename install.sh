68cf3f57059d0b19ee73396341f11ed4ef9f7351
Richard Dong <66289396+rdong8@users.noreply.github.com>
2026-09-08 23:30:42 -0400

    fix: stow restore

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
-# Primarily intended for use by devcontainers
+#!/usr/bin/env fish
+
+# This script is intended for use by devcontainers
+
+# --adopt + restore forces the dotfiles from this repo to override any existing ones in the devcontainer
 stow --adopt -v -t ~ .
+git restore .
